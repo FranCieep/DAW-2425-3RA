a83ffa7f0e1ae8bfbabd1a73393b0b3c50d87655
bibis <bertha@gmail.com>
2025-03-12 11:31:16 +0100

    Add footer, navbar, and principal components with styles and templates; include Bootstrap in project

--- Angular-II/Proyecto-Base-II/src/app/app.component.ts
+++ Angular-II/Proyecto-Base-II/src/app/app.component.ts
@@ -7,6 +7,32 @@ import { RouterOutlet } from '@angular/router';
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
-export class AppComponent {
+export class AppComponent{
   title = 'Proyecto-Base-II';
+
+  name = 'Bertha';
+  direccion = 'Calle 123';
+  edad = 25;
+  altura = 1.60;
+  estado = true;
+  frutas: string[] =[ 'manzana', 'pera', 'uva', 'sandia', 'melon', 'aguacate'];
+  notas = [1,2,3,4,5,6,7,8,9,10];
+  fruta={
+    nombre: 'manzana',
+    color: 'rojo',
+    precio: 1.50
+  }
+
+  masUno(): number{
+     return this.edad += 1;
+  }
+  cambairEstado(): void{
+    this.estado= false
+    this.fruta={
+      nombre: 'mandarina',
+      color: 'naranja',
+      precio: 2.50  
+    }
+  }
+
 }
